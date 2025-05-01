2bbcd1daaa5db5d9c0cf5fe789ec18f1d07acc3e
AlyaAllsoushaRR <albisharafikova@yandex.ru>
2025-05-01 18:42:37 +0300

    Albina

--- app/src/main/java/com/example/foodtogether/MainActivity.kt
+++ app/src/main/java/com/example/foodtogether/MainActivity.kt
@@ -3,18 +3,28 @@ package com.example.foodtogether
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.Text
 
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 
 import androidx.compose.ui.unit.dp
 
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.foodtogether.ui.theme.FoodTogetherTheme
 
 
 class MainActivity : ComponentActivity() {
@@ -22,19 +32,40 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Main()
+            FoodTogetherTheme(darkTheme = isSystemInDarkTheme()) {
+                Main()
+            }
+
         }
     }
 }
 @Composable
 fun Main() {
     val navController = rememberNavController()
-    Column(Modifier.padding(8.dp)) {
+    Column() {
+        Button(onClick = {}) {
+            Text(text ="Text")
+        }
         NavHost(navController, startDestination = NavRoutes.Home.route, modifier = Modifier.weight(1f)) {
             composable(NavRoutes.Home.route) { Home() }
             composable(NavRoutes.Contacts.route) { Contacts()  }
             composable(NavRoutes.About.route) { About() }
         }
         BottomNavigationBar(navController = navController)
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun LightPreview() {
+    FoodTogetherTheme(darkTheme = false) {
+        Main()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DarkPreview() {
+    FoodTogetherTheme(darkTheme = true) {
+        Main()
     }
 }
--- app/src/main/java/com/example/foodtogether/ui/theme/Theme.kt
+++ app/src/main/java/com/example/foodtogether/ui/theme/Theme.kt
@@ -9,43 +9,50 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+    primary = OrangeChoosse80,
+    secondary = Pink80,
+    tertiary = Pink40,
+    surface =Pink80,
+
+    primaryContainer = OrangeChoosse80, //выделенный
+    onPrimary = black80,//цвет кнопки
+    onTertiary = black80,
+    onBackground = black80,
+    onSurface = black80,
+
+
+    )
+
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = OrangeChoosse80, //кнопки
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onPrimary = black80,//цвет кнопки
+    primaryContainer = OrangeChoosse80, //выделенный
+
+    secondary = Pink80,
+    tertiary = Pink40,
+
+    background = Pink80,
+    surface = Pink80,
+    onSecondary = black80,
+    onTertiary = black80,
+    onBackground = black80,
+    onSurface = black80 //цвет активной панели навигации
+
 )
 
 @Composable
 fun FoodTogetherTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
